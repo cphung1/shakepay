4268c2015a6b747425e80a6e4a8237fb49397707
cphung1 <cphung97@gmail.com>
2020-07-15 12:10:29 -0600

    data persists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import moment from 'moment';
 
 import './App.css';
@@ -18,11 +18,10 @@ function App() {
     ETH_CAD_rate: null,
   })
 
+
   useEffect(() => {
     axios.get('https://cors-anywhere.herokuapp.com/https://api.shakepay.co/rates')
       .then(response => {
-        // BTC_CAD_rate = response.data.BTC_CAD;
-        // ETH_CAD_rate = response.data.ETH_CAD;
         setState({ BTC_CAD_rate: response.data.BTC_CAD, ETH_CAD_rate: response.data.ETH_CAD})
 
         axios.get('https://shakepay.github.io/programming-exercise/web/transaction_history.json')
@@ -31,6 +30,12 @@ function App() {
           response.data.reverse().forEach((element, idx) => {
             if (element.type !== 'conversion') {
               labels.push(moment(element.createdAt).format('YYYY-MM-DD'))
+
+              // const labelsCopy = state.labels;
+              // labelsCopy.push(moment(element.createdAt).format('YYYY-MM-DD'))
+              // setState(prev => ({...prev, labels: labelsCopy}))
+              // const labelsCopy = state.labels.concat([moment(element.createdAt).format('YYYY-MM-DD')])
+              // setState(prev => ({labels: [...prev.labels, moment(element.createdAt).format('YYYY-MM-DD')]}))
 
               if (idx === 0 ) {
                 networth += element.amount;
@@ -52,8 +57,17 @@ function App() {
                 }
               }
               data.push(networth);
+
+              // const dataCopy = state.data;
+              // dataCopy.push(networth)
+              // setState(prev => ({...prev, data: dataCopy}))
             }
           })
+
+          setState({ labels: labels, data: data})
+        })
+        .catch(err => {
+          console.log(err)
         })
 
       })
@@ -65,8 +79,8 @@ function App() {
   return (
     <div className="App">
         <Chart 
-          labels={labels}
-          data={data}
+          labels={state.labels}
+          data={state.data}
         />
     </div>
   );
